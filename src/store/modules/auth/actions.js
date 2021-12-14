ffb778bbf0d7f803ea9cfa20338d5438bb762b9c
Marin Leontenko <marin.leontenko@gmail.com>
2021-12-14 09:01:07 +0100

    refactor auth

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,31 +1,27 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArs7eEV4KnVowdYQDX501y1PFvtogF3EQ', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
-            throw error;
-        }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         });
     },
     async signup(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyArs7eEV4KnVowdYQDX501y1PFvtogF3EQ', {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArs7eEV4KnVowdYQDX501y1PFvtogF3EQ';
+
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyArs7eEV4KnVowdYQDX501y1PFvtogF3EQ';
+        }
+
+        const response = await fetch(
+            url, 
+            {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -41,8 +37,7 @@ export default {
             const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
             throw error;
         }
-
-        console.log(responseData);
+        
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
