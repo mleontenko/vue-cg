30cc8f12171f03aaedc375500608946c29888935
Marin Leontenko <marin.leontenko@gmail.com>
2021-12-14 09:25:26 +0100

    auto login, isAuthenticated bugfix

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -37,12 +37,27 @@ export default {
             const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
             throw error;
         }
-        
+
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         });
+    },
+    tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
+        }
     },
     logout(context) {
         context.commit('setUser', {
